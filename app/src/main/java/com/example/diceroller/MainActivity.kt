9cfb22a66fafa78bb12444161645a818ccaf3236
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2023-08-29 16:35:05 +0600

    First kotlin android app

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var diceImage: ImageView
+    private lateinit var countButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
+        countButton = findViewById(R.id.countButton)
+
+        countButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+
+        val drawableResource = when(Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+    }
+
+
+}
